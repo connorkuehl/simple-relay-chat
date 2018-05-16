a506704f9b17cfdcbf07fc99f3f860b6e7ed5f59
Connor Kuehl <cipkuehl@gmail.com>
2018-05-16 12:40:16 -0700

    Add basic identification of client

--- srcp/server/src/client.rs
+++ srcp/server/src/client.rs
@@ -1,9 +1,27 @@
 use ::net;
-use ::{Arc, Mutex};
+use ::Read;
+use ::{Arc, Mutex, Weak};
 
 #[derive(Debug)]
-pub struct Client;
+pub struct Client {
+    pub user: String,
+    pub conn: Weak<Mutex<net::TcpStream>>,
+}
 
-pub fn identify(conn: &mut net::TcpStream, clients: &Arc<Mutex<Vec<Client>>>) -> Result<Client, String> {
-    Ok(Client {})
+pub fn identify(conn: &Arc<Mutex<net::TcpStream>>, clients: &Arc<Mutex<Vec<Client>>>) -> Result<Client, String> {
+    let mut buffer = String::new();
+
+    conn.lock().unwrap().read_to_string(&mut buffer).unwrap();
+    let words: Vec<&str> = buffer.split(" ").collect();
+    if words.len() != 2 {
+        return Err("malformed".into());
+    }
+    let username = words[1].trim();
+
+    let client = Client {
+        user: username.into(),
+        conn: Arc::downgrade(&conn),
+    };
+
+    Ok(client)
 }
