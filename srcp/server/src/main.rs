09dda0b9f6c362cacd558143be6df3ead6c623da
Connor Kuehl <cipkuehl@gmail.com>
2018-05-16 12:41:13 -0700

    Add client table, mandatory identification, and event pipeline

--- srcp/server/src/main.rs
+++ srcp/server/src/main.rs
@@ -5,35 +5,38 @@ use threadpool::ThreadPool;
 use std::net;
 use std::thread;
 use std::sync::mpsc;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, Weak};
 use std::io::Read;
 use event::Event;
+use client::Client;
 
 mod client;
 mod event;
 
 const NCLIENT: usize = 32;
 
+struct Command {
+    sender: Arc<Client>,
+    cmd_msg: String,
+}
+
+fn execute(cmd: Event, clients: &Arc<Mutex<Vec<Client>>>) -> Result<(), String> {
+    Ok(())
+}
+
 fn main() {
     let listener = net::TcpListener::bind("127.0.0.1:6667").unwrap();
 
-    let (sender, receiver) = mpsc::channel();
+    let (sender, receiver): (mpsc::Sender<Command>, mpsc::Receiver<Command>) = mpsc::channel();
 
-    let clients = Arc::new(Mutex::new(vec![]));
+    let clients: Arc<Mutex<Vec<Client>>> = Arc::new(Mutex::new(vec![]));
 
     let allclients = Arc::downgrade(&clients);
     let events = thread::spawn(move || {
         println!("Event thread online.");
 
-        for received in receiver {
-            match received {
-                Event::Identify(client) => {
-                    if let Some(clients) = allclients.upgrade() {
-                        clients.lock().unwrap().push(client);
-                    }
-                },
-                _ => (),
-            }
+        for message in receiver {
+            println!("{}: {}", message.sender.user, message.cmd_msg);
         }
     });
 
@@ -43,28 +46,36 @@ fn main() {
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
+        let stream = Arc::new(Mutex::new(stream));
         println!("Incoming connection!");
 
-        let allclients = Arc::downgrade(&clients);
         let sndr = sender.clone();
+        let connected = Arc::clone(&clients); 
+
         pool.execute(move || {
-            let connected = allclients.upgrade().unwrap();
-            if let Ok(client) = client::identify(&mut stream, &connected) {
-                sndr.send(Event::Identify(client)).unwrap();
-            } else {
-                stream.shutdown(net::Shutdown::Both).unwrap();
-            }
+            // Mandatory identification step before able to participate in relay
+            let client = Arc::new(client::identify(&stream, &connected).unwrap());
+            let identified = Command {
+                sender: Arc::clone(&client),
+                cmd_msg: format!("/IDENTIFY {}", client.user),
+            };
+
+            sndr.send(identified).unwrap();
 
             loop {
                 let mut message = String::new();
-                if let Ok(bytes_read) = stream.read_to_string(&mut message) {
+                if let Ok(bytes_read) = stream.lock().unwrap().read_to_string(&mut message) {
                     if bytes_read > 0 {
-                        sndr.send(event::parse(message).unwrap()).unwrap();
+                        let cmd = Command {
+                            sender: Arc::clone(&client),
+                            cmd_msg: message,
+                        };
+                        sndr.send(cmd).unwrap();
                     } else {
                         break;
                     }
                 } else {
-                    stream.shutdown(net::Shutdown::Both).unwrap();
+                    stream.lock().unwrap().shutdown(net::Shutdown::Both).unwrap();
                     break;
                 }
             }
