a03cf0221016eef714ac6a79ef3ade532fb4e82b
Connor Kuehl <cipkuehl@gmail.com>
2018-05-15 19:28:43 -0700

    Main thread listens for incoming connections

--- srcp/server/src/main.rs
+++ srcp/server/src/main.rs
@@ -1,3 +1,13 @@
+use std::net;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = net::TcpListener::bind("127.0.0.1:6667").unwrap();
+
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        println!("Incoming connection!");
+        stream.write(b"Hello!\n");
+        stream.flush().unwrap();
+    }
 }
