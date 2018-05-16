e7d61f72fd496f181c2fe02af5635b344c4de92c
Connor Kuehl <cipkuehl@gmail.com>
2018-05-15 21:41:19 -0700

    Create listen thread for each client

--- srcp/server/src/main.rs
+++ srcp/server/src/main.rs
@@ -1,7 +1,13 @@
+extern crate threadpool;
+
+use threadpool::ThreadPool;
+
 use std::net;
 use std::thread;
 use std::sync::mpsc;
-use std::io::Write;
+use std::io::Read;
+
+const NCLIENT: usize = 32;
 
 fn main() {
     let listener = net::TcpListener::bind("127.0.0.1:6667").unwrap();
@@ -16,16 +22,25 @@ fn main() {
         }
     });
 
+    let pool = ThreadPool::new(NCLIENT);
+
     println!("Waiting for connections...");
 
     for stream in listener.incoming() {
         let mut stream = stream.unwrap();
         println!("Incoming connection!");
-        stream.write(b"Hello!\n");
-        stream.flush().unwrap();
 
-        sender.send("Message sent").unwrap();
+        let sndr = sender.clone();
+        pool.execute(move || {
+            loop {
+                let mut message = String::new();
+                if stream.read_to_string(&mut message).unwrap() > 0 {
+                    sndr.send(message).unwrap();
+                }
+            }
+        });
     }
 
     events.join().unwrap();
+    pool.join();
 }
