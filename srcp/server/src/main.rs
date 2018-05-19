2c23bf96b8297b496e26e17c71b85aa44f2f54f4
Connor Kuehl <cipkuehl@gmail.com>
2018-05-18 17:42:41 -0700

    Refactor client handling to own function

--- srcp/server/src/main.rs
+++ srcp/server/src/main.rs
@@ -9,11 +9,29 @@ use std::io::{Read, Write};
 const NCLIENT: usize = 32;
 const MSGSIZE: usize = 1024;
 
+fn handle_client(stream: net::TcpStream) {
+    let mut stream = stream;
+    loop {
+        let mut message = [0; MSGSIZE];
+        if let Ok(bytes_read) = stream.read(&mut message) {
+            if bytes_read > 0 {
+                let msg = message.to_vec();
+                let msg = String::from_utf8_lossy(&msg);
+                let msg = msg.trim_right();
+                println!("got '{}'", msg);
+                stream.write(&message).expect("echo");
+                stream.flush().expect("echo flush");
+            } else {
+                break;
+            }
+        }
+
+        println!("Client disconnected.");
+    }
+}
+
 fn main() {
     let listener = net::TcpListener::bind("0.0.0.0:6667").expect("bind");
-
-    //let (sender, receiver): (mpsc::Sender<Command>, mpsc::Receiver<Command>) = mpsc::channel();
-    //let (sender, receiver) = mpsc::channel();
 
     let events = thread::spawn(move || {
         println!("Event thread online.");
@@ -24,27 +42,14 @@ fn main() {
     println!("Waiting for connections...");
 
     for stream in listener.incoming() {
-        let mut stream = stream.expect("incoming");
-        println!("Incoming connection!");
-
-        pool.execute(move || {
-            loop {
-                let mut message = [0; MSGSIZE];
-                if let Ok(bytes_read) = stream.read(&mut message) {
-                    if bytes_read > 0 {
-                        let msg = message.to_vec();
-                        let msg = String::from_utf8_lossy(&msg);
-                        println!("got '{}'", msg);
-                        stream.write(&message).expect("echo");
-                        stream.flush().expect("echo flush");
-                    } else {
-                        break;
-                    }
-                }
-            }
-
-            println!("Client disconnected.");
-        });
+        if let Ok(stream) = stream {
+            println!("Incoming connection!");
+            pool.execute(move || {
+                handle_client(stream);
+            });
+        } else {
+            eprintln!("Failed to accept incoming connection.");
+        }
     }
 
     events.join().expect("events join");
