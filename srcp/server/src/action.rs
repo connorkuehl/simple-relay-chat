fe49e9be7df3d14d2947698932ae699f488ab493
Connor Kuehl <cipkuehl@gmail.com>
2018-05-19 10:29:45 -0700

    WIP begin implementing action handler

--- srcp/server/src/action.rs
+++ srcp/server/src/action.rs
@@ -1,9 +1,34 @@
+use ::net;
+use ::Client;
+use ::HashMap;
+use ::std::io::Write;
+
 use ::event::{Event, EventKind};
 
-pub fn execute(event: Event) {
-    let mut retcode = 0;
+const OK: usize = 0;
+
+pub fn execute(event: Event, peers: &mut HashMap<net::SocketAddr, Client>) {
+    let mut retcode = OK;
+    let mut event = event;
 
     match event.kind {
-        _ => (),
+        EventKind::Identify(user) => {
+            if let Ok(addr) = event.from.peer_addr() {
+                if !peers.contains_key(&addr) {
+                    let client = Client {
+                        user: user,
+                        conn: event.from.try_clone().expect("try_clone"),
+                    };
+                    peers.insert(addr, client);
+                } else {
+                }
+            }
+        },
+        _ => retcode = 999,
     }
+
+    let response = format!("{} {}", retcode, event.contents);
+
+    event.from.write(response.as_bytes()).expect("write");
+    event.from.flush().expect("write flush");
 }
