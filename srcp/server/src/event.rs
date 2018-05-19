d15b62792170e7f98a173a42f0f92737dd198624
Connor Kuehl <cipkuehl@gmail.com>
2018-05-18 20:42:31 -0700

    Add module for message to event parsing

---
+++ srcp/server/src/event.rs
@@ -0,0 +1,29 @@
+use ::net;
+
+pub enum EventKind {
+    Identify(String),
+    Error(String),
+}
+
+pub struct Event {
+    pub from: net::TcpStream,
+    pub kind: EventKind,
+}
+
+pub fn kind_parse(s: &String) -> EventKind {
+    let command = match s.split_whitespace().nth(0) {
+        Some(first) => first,
+        None => return EventKind::Error("no command".into()),
+    };
+
+    let args = s.split_whitespace().skip(1).next();
+
+    match command {
+        "IDENTIFY" => identify(args.unwrap()),
+        _ => EventKind::Error("command not recognized".into()),
+    }
+}
+
+fn identify(args: &str) -> EventKind {
+    EventKind::Identify(args.into())    
+}
