5b0ebd8f11dee9ea66fa9b73da00c0ff6cd6ae4e
Connor Kuehl <cipkuehl@gmail.com>
2018-05-21 16:52:11 -0700

    Add Say command

--- srcp/server/src/action.rs
+++ srcp/server/src/action.rs
@@ -6,9 +6,11 @@ use ::Client;
 use ::event::{Event, EventKind};
 
 const OK: usize = 0;
+/*
 const ROOM_DOESNT_EXIST: usize = 1;
 const USER_DOESNT_EXIST: usize = 2;
 const POORLY_FORMED_COMMAND: usize = 3;
+*/
 const USERNAME_UNAVAILABLE: usize = 4;
 
 pub fn execute(event: Event, peers: &mut Vec<Client>) {
@@ -18,13 +20,14 @@ pub fn execute(event: Event, peers: &mut Vec<Client>) {
         EventKind::Identify(_) => on_identify(&mut event, peers),
         EventKind::Join(_) => on_join(&mut event, peers),
         EventKind::List(_) => on_list(&mut event, peers),
+        EventKind::Say(_, _) => on_say(&mut event, peers),
         EventKind::Quit => on_quit(&mut event, peers),
         _ => (999, String::from("Unknown")),
     };
 
     let response = format!("{} {}\n", retcode, reply);
-    event.from.write(response.as_bytes());
-    event.from.flush();
+    event.from.write(response.as_bytes()).expect("write");
+    event.from.flush().expect("flush");
 }
 
 fn on_identify(event: &mut Event, peers: &mut Vec<Client>) -> (usize, String) {
@@ -72,7 +75,6 @@ fn on_list(event: &mut Event, peers: &mut Vec<Client>) -> (usize, String) {
 
     let reply = match to_list {
         Some(room) => {
-            println!("LIST on '{}'", room);
             let clients: Vec<String> = peers.iter()
                 .filter(|c| c.rooms.iter().any(|r| r.eq(room)))
                 .map(|c| c.user.clone())
@@ -99,6 +101,32 @@ fn on_list(event: &mut Event, peers: &mut Vec<Client>) -> (usize, String) {
     };
 
     (OK, reply)
+}
+
+fn on_say(event: &mut Event, peers: &mut Vec<Client>) -> (usize, String) {
+    let (room, message) = match &event.kind {
+        EventKind::Say(r, m) => (r, m),
+        _ => panic!("on_say received non-say event"),
+    };
+
+    let addr = event.from.peer_addr().expect("peer_addr");
+    let sender = match peers.iter().position(|x| x.conn.peer_addr().expect("peer_addr").eq(&addr)) {
+        Some(index) => peers[index].user.clone(),
+        None => panic!("user"),
+    };
+
+    let final_msg = format!("{} {} {} {}\n", OK, sender, room, message);
+
+    let send_to = peers.into_iter()
+        .filter(|p| p.rooms.iter().any(|r| r.eq(room)));
+
+    for client in send_to {
+        client.conn.write(final_msg.as_bytes()).expect("write");
+        client.conn.flush().expect("flush");
+    }
+
+
+    (OK, event.contents.clone())
 }
 
 fn on_quit(event: &mut Event, peers: &mut Vec<Client>) -> (usize, String) {
