212cdf092d290a7b73256ed07fbac4a151733aa6
Connor Kuehl <cipkuehl@gmail.com>
2018-05-26 12:02:03 -0700

    Add identify assertion

--- srcp/server/src/server.rs
+++ srcp/server/src/server.rs
@@ -6,6 +6,24 @@ use ::std::collections::{HashSet, HashMap};
 use ::Event;
 use ::Command;
 
+macro_rules! assert_identified {
+    ( $x: expr, $y: expr ) => {
+        {
+            let temp_index = $x.iter()
+                .position(|c| c.connection.peer_addr().expect("peer_addr").eq(&$y.peer_addr().expect("peer_addr")));
+
+            if temp_index.is_none() {
+                ignore_result($y.write(&format!("9 {}\n", "UNIDENTIFIED").as_bytes()));
+                ignore_result($y.flush());
+                ignore_result($y.shutdown(net::Shutdown::Both));
+                return;
+            } else {
+                temp_index.unwrap()
+            }
+        }
+    };
+}
+
 pub struct Client {
     pub name: String,
     pub connection: net::TcpStream,
@@ -35,10 +53,7 @@ impl Server {
         }
     }
 
-    pub fn exec(&mut self, event: Event) {
-        let sender_index = self.clients.iter()
-            .position(|c| c.connection.peer_addr().expect("peer_addr").eq(&event.from.peer_addr().expect("peer_addr")));
-
+    pub fn exec(&mut self, mut event: Event) {
         let reply = match event.command {
             Command::Identify(username) => {
                 if self.clients.iter().any(|c| c.name.eq(&username)) {
@@ -53,93 +68,94 @@ impl Server {
 
                 event.raw
             },
-            Command::Join(room) => {
-                let index = sender_index.unwrap();
-                self.clients[index].rooms.insert(room.clone());
+            _ => { 
+                let index = assert_identified!(self.clients, event.from);
+                match event.command {
+                    Command::Join(room) => {
+                        self.clients[index].rooms.insert(room.clone());
 
-                let list = self.rooms.entry(room.clone()).or_insert(vec![]);
-                list.push(self.clients[index].clone());
+                        let list = self.rooms.entry(room.clone()).or_insert(vec![]);
+                        list.push(self.clients[index].clone());
 
-                let joinmsg = Server::create_message(
-                    0, 
-                    &format!("{} has joined.", self.clients[index].name), 
-                    "server", 
-                    &room
-                );
-                Server::say(list.as_mut_slice(), &joinmsg);
+                        let joinmsg = Server::create_message(
+                            0, 
+                            &format!("{} has joined.", self.clients[index].name), 
+                            "server", 
+                            &room
+                        );
+                        Server::say(list.as_mut_slice(), &joinmsg);
 
-                event.raw
-            },
-            Command::List(room) => {
-                // user provided a room name
-                if let Some(room) = room {
-                    match self.rooms.get(&room) {
-                        // room exists
-                        Some(rm) => {
-                            let usernames: Vec<String> = rm.iter().map(|c| c.name.clone()).collect();
-                            usernames.join(" ")
-                        },
-                        None => {
-                            // Error
-                            String::from("room doesn't exist")
-                        },
-                    }
-                } else {
-                    let rooms: Vec<String> = self.rooms.keys().map(|k| k.clone()).collect();
-                    rooms.join(" ")
-                }
-            },
-            Command::Say(room, message) => {
-                let index = sender_index.unwrap();
-                let name = self.clients[index].name.clone();
-                if let Some(recipients) = self.rooms.get_mut(&room) {
-                    let message = Server::create_message(0, &message, &name, &room);
-                    Server::say(recipients.as_mut_slice(), &message);
-                }
-
-                event.raw
-            },
-            Command::Leave(room) => {
-                let index = sender_index.unwrap();
-                let user = self.clients[index].name.clone();
-
-                if self.clients[index].rooms.contains(&room) {
-                    if let Some(subscribed) = self.rooms.get_mut(&room) {
-                        let message = Server::create_message(0, &format!("{} has left.", user), "server", &room);
-                        Server::say(subscribed.as_mut_slice(), &message);
-                    }
-
-                    self.clients[index].rooms.remove(&room);
-                }
-
-                event.raw
-            },
-            Command::Quit => {
-                let index = sender_index.unwrap();
-                let client = self.clients[index].clone();
-                let user = client.name.clone();
-
-                for room in client.rooms {
-                    if let Some(subscribed) = self.rooms.get_mut(&room) {
-                        // channel exists, notify clients that user is leaving
-                        let message = Server::create_message(0, &format!("{} has left.", &user), "server", &room);
-                        Server::say(subscribed.as_mut_slice(), &message);
-
-                        // remove this user from the room's list of subscribed clients
-                        if let Some(pos) = subscribed.iter().position(|c| c.name.eq(&user)) {
-                            subscribed.remove(pos);
+                        event.raw
+                    },
+                    Command::List(room) => {
+                        // user provided a room name
+                        if let Some(room) = room {
+                            match self.rooms.get(&room) {
+                                // room exists
+                                Some(rm) => {
+                                    let usernames: Vec<String> = rm.iter().map(|c| c.name.clone()).collect();
+                                    usernames.join(" ")
+                                },
+                                None => {
+                                    // Error
+                                    String::from("room doesn't exist")
+                                },
+                            }
+                        } else {
+                            let rooms: Vec<String> = self.rooms.keys().map(|k| k.clone()).collect();
+                            rooms.join(" ")
                         }
-                    }
+                    },
+                    Command::Say(room, message) => {
+                        let name = self.clients[index].name.clone();
+                        if let Some(recipients) = self.rooms.get_mut(&room) {
+                            let message = Server::create_message(0, &message, &name, &room);
+                            Server::say(recipients.as_mut_slice(), &message);
+                        }
+
+                        event.raw
+                    },
+                    Command::Leave(room) => {
+                        let user = self.clients[index].name.clone();
+
+                        if self.clients[index].rooms.contains(&room) {
+                            if let Some(subscribed) = self.rooms.get_mut(&room) {
+                                let message = Server::create_message(0, &format!("{} has left.", user), "server", &room);
+                                Server::say(subscribed.as_mut_slice(), &message);
+                            }
+
+                            self.clients[index].rooms.remove(&room);
+                        }
+
+                        event.raw
+                    },
+                    Command::Quit => {
+                        let client = self.clients[index].clone();
+                        let user = client.name.clone();
+
+                        for room in client.rooms {
+                            if let Some(subscribed) = self.rooms.get_mut(&room) {
+                                // channel exists, notify clients that user is leaving
+                                let message = Server::create_message(0, &format!("{} has left.", &user), "server", &room);
+                                Server::say(subscribed.as_mut_slice(), &message);
+
+                                // remove this user from the room's list of subscribed clients
+                                if let Some(pos) = subscribed.iter().position(|c| c.name.eq(&user)) {
+                                    subscribed.remove(pos);
+                                }
+                            }
+                        }
+
+                        ignore_result(client.connection.shutdown(net::Shutdown::Both));
+
+                        // remove from list of clients
+                        self.clients.remove(index);
+
+                        event.raw
+                    },
+                    _ => String::from("unknown"),
                 }
-
-                ignore_result(client.connection.shutdown(net::Shutdown::Both));
-
-                // remove from list of clients
-                self.clients.remove(index);
-
-                event.raw
-            },
-            _ => String::from("unknown"),
+            }
         };
 
         let reply = Server::create_message(0, &reply, "server", "server");
