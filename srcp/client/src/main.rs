bb458c99eb1b6c65e20e9864bc4536a18bef392f
Connor Kuehl <cipkuehl@gmail.com>
2018-06-03 20:52:59 -0700

    Pull in ncurses crate

--- srcp/client/src/main.rs
+++ srcp/client/src/main.rs
@@ -1,3 +1,16 @@
+extern crate ncurses;
+
+use ncurses::*;
+
 fn main() {
-    println!("Hello, world!");
+    initscr();
+    noecho();
+
+    let mut scr_width  = 0;
+    let mut scr_height = 0;
+    getmaxyx(stdscr(), &mut scr_width, &mut scr_height);
+    
+
+    getch();
+    endwin();
 }
