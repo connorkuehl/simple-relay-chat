2a486e8be48b50e83766ec4b3ef4da4d071aaca9
Connor Kuehl <cipkuehl@gmail.com>
2018-06-04 20:16:50 -0700

    Add basic input loop that echoes into chat box

--- srcp/client/src/main.rs
+++ srcp/client/src/main.rs
@@ -15,8 +15,7 @@ fn mkwin(lines: i32, cols: i32, row: i32, col: i32) -> ncurses::WINDOW {
 
 fn main() {
     initscr();
-    noecho();
-
+    
     let mut scr_width  = 0;
     let mut scr_height = 0;
     getmaxyx(stdscr(), &mut scr_height, &mut scr_width);
@@ -28,9 +27,28 @@ fn main() {
     let chat_window = mkwin(scr_height - INPUT_WINDOW_HEIGHT as i32, scr_width - ROOM_WINDOW_WIDTH as i32, 0, ROOM_WINDOW_WIDTH as i32);
 
     let input_window = mkwin(INPUT_WINDOW_HEIGHT as i32, scr_width, scr_height - INPUT_WINDOW_HEIGHT as i32, 0);
+    let mut input_row = scr_height - INPUT_WINDOW_HEIGHT as i32 - 1;
+    let mut input_col = 0;
+    keypad(input_window, true);
+
+    loop {
+        wmove(input_window, 1, 1);
+        wrefresh(input_window);
+
+        let mut input = String::new();
+        wgetstr(input_window, &mut input);
+        wclear(input_window);
+        box_(input_window, 0, 0);
+        wrefresh(input_window);
+        
+        mvwaddstr(chat_window, 1, 1, &input);
+        wrefresh(chat_window);
+    }
 
     getch();
 
+    delwin(room_window);
+    delwin(chat_window);
     delwin(input_window);
     endwin();
 }
