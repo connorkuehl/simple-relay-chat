553838c6084c55ce5ebe688b2ef109ab5929a14a
Connor Kuehl <cipkuehl@gmail.com>
2018-06-06 20:22:35 -0700

    Add server constructor

--- srcp/client/src/server.rs
+++ srcp/client/src/server.rs
@@ -1,1 +1,16 @@
-pub struct Server;
+use ::std;
+use std::net;
+
+pub struct Server {
+    conn: net::TcpStream,
+}
+
+impl Server {
+    pub fn new(addr: &str) -> std::io::Result<Server> {
+        let stream = net::TcpStream::connect(addr)?;
+
+        Ok(Server {
+            conn: stream,
+        })
+    }
+}
